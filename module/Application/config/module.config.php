7561bdf658e4012fbed465e225f16b99f614a2b1
igoryen <igor.yentaltsev@gmail.com>
2015-02-04 15:12:53 -0500

    bug20 - home page displays list of terms

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -15,7 +15,7 @@ return array(
                 'options' => array(
                     'route'    => '/',
                     'defaults' => array(
-                        'controller' => 'Application\Controller\Index',
+                        'controller' => 'Term\Controller\Term',
                         'action'     => 'index',
                     ),
                 ),
