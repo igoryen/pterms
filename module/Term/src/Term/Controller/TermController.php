01a9e3480d81cc3d102719a8d0b4bf600dffd8f7
igoryen <igor.yentaltsev@gmail.com>
2015-02-04 14:30:44 -0500

    bug17 - Can add new terms

--- module/Term/src/Term/Controller/TermController.php
+++ module/Term/src/Term/Controller/TermController.php
@@ -4,6 +4,8 @@ namespace Term\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Term\Model\Term;
+use Term\Form\TermForm;
 
 class TermController extends AbstractActionController {
   
@@ -16,7 +18,24 @@ class TermController extends AbstractActionController {
   }
 
   public function addAction() {
-    
+    $form = new TermForm();
+    $form->get('submit')->setValue('Add');
+
+    $request = $this->getRequest();
+    if ($request->isPost()) {
+      $term = new Term();
+      $form->setInputFilter($term->getInputFilter());
+      $form->setData($request->getPost());
+
+      if ($form->isValid()) {
+        $term->exchangeArray($form->getData());
+        $this->getTermTable()->saveTerm($term);
+
+        // Redirect to list of terms
+        return $this->redirect()->toRoute('term');
+      }
+    }
+    return array('form' => $form);
   }
 
   public function editAction() {
