5c2aa818d00daed47c88d41f3e022e7413753232
igoryen <igor.yentaltsev@gmail.com>
2015-02-03 22:51:20 -0500

    bug7 - Routing and controllers

--- module/Term/config/module.config.php
+++ module/Term/config/module.config.php
@@ -6,6 +6,26 @@ return array(
       'Term\Controller\Term' => 'Term\Controller\TermController',
     ),
   ),
+  
+  'router' => array(
+    'routes' => array(
+      'album' => array(
+        'type' => 'segment',
+        'options' => array(
+          'route' => '/term[/:action][/:id]',
+          'constraints' => array(
+            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+            'id' => '[0-9]+',
+          ),
+          'defaults' => array(
+            'controller' => 'Term\Controller\Term',
+            'action' => 'index',
+          ),
+        ),
+      ),
+    ),
+  ),
+  
   'view_manager' => array(
     'template_path_stack' => array(
       'album' => __DIR__ . '/../view',
