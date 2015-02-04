99b1f13420f3efdea4943329a1f9e3a71184a646
igoryen <igor.yentaltsev@gmail.com>
2015-02-04 10:27:33 -0500

    bug8 - TermController: made

---
+++ module/Term/src/Term/Controller/TermController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Term\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class TermController extends AbstractActionController {
+
+  public function indexAction() {
+    
+  }
+
+  public function addAction() {
+    
+  }
+
+  public function editAction() {
+    
+  }
+
+  public function deleteAction() {
+    
+  }
+
+}
