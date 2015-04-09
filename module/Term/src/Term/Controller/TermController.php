ea2e4e7015a1dbd471626eaaebd2079ff48b7243
igoryen <igor.e@advertiseit.com>
2015-04-09 17:02:52 -0400

    Number of items per page is now 50

--- module/Term/src/Term/Controller/TermController.php
+++ module/Term/src/Term/Controller/TermController.php
@@ -16,8 +16,8 @@ class TermController extends AbstractActionController {
      $paginator = $this->getTermTable()->fetchAll(true);
      // set the current page to what has been passed in query string, or to 1 if none set
      $paginator->setCurrentPageNumber((int) $this->params()->fromQuery('page', 1));
-     // set the number of items per page to 10
-     $paginator->setItemCountPerPage(10);
+     // set the number of items per page to 50
+     $paginator->setItemCountPerPage(50);
     return new ViewModel(array(
       'paginator' => $paginator
     ));
